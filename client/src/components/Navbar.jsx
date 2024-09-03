1ff736b905062bc5ebb2409ca1e08ca0938eafef
Saminathan-77 <csaminathan2023@gmail.com>
2024-09-04 00:22:26 +0530

    modifed html pages

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,18 +1,18 @@
 // src/components/Navbar.jsx
-import React from 'react';
-import { useNavigate } from 'react-router-dom';
-import '../index.css';
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import "../index.css";
 
 const Navbar = () => {
   const navigate = useNavigate();
 
   const handleLoginClick = () => {
-    navigate('/login');
+    navigate("/login");
   };
 
   return (
     <nav className="navbar">
-      <h1>Photo Analysis App</h1>
+      <h1>PixelPerfect</h1>
       <button className="login-button" onClick={handleLoginClick}>
         Login
       </button>
