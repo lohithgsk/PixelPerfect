c5fa7e0a4b824c5274fa2b67b026083f92a790b2
EgoisticPotato <egoistic.potato@gmail.com>
2024-09-04 23:32:26 +0530

    Made changes to the navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -22,11 +22,7 @@ const Navbar = () => {
   };
 
   return (
-<<<<<<< HEAD
     <nav className="relative bg-gray-800 text-white flex justify-between items-center px-4 py-2 overflow-hidden transition-all duration-300 ease-in-out hover:bg-gray-700">
-=======
-    <nav className="relative bg-gray-800 text-white flex justify-between items-center px-4 py-3 overflow-hidden transition-all duration-300 ease-in-out hover:bg-gray-700">
->>>>>>> 16d1ec8ab938ad13c48d0b603aca8781ed8447bf
       {/* Streak line effect */}
       <div className="absolute inset-0 flex justify-center items-center">
         <div className="absolute inset-0 bg-gradient-to-r from-transparent via-gray-900 to-transparent opacity-20" />
@@ -41,27 +37,13 @@ const Navbar = () => {
       </h1>
       <div className="flex space-x-2 z-10">
         <button
-<<<<<<< HEAD
           className="bg-blue-500 hover:bg-blue-600 text-white py-2 px-4 rounded-full shadow-md hover:shadow-lg transform transition-transform duration-300 ease-in-out hover:scale-105 text-sm"
-=======
-          className="bg-blue-500 hover:bg-blue-600 text-white py-2 px-4 ml-1 rounded-full shadow-md hover:shadow-lg transform transition-transform duration-300 ease-in-out hover:scale-105 text-sm"
->>>>>>> 16d1ec8ab938ad13c48d0b603aca8781ed8447bf
           onClick={handleUploadClick}
         >
           Upload
         </button>
         <button
-<<<<<<< HEAD
           className="bg-blue-500 hover:bg-blue-600 text-white py-2 px-4 rounded-full shadow-md hover:shadow-lg transform transition-transform duration-300 ease-in-out hover:scale-105 text-sm"
-=======
-          className="bg-blue-500 hover:bg-blue-600 text-white ml-1 mr-1 py-2 px-4 rounded-full shadow-md hover:shadow-lg transform transition-transform duration-300 ease-in-out hover:scale-105 text-sm"
-          onClick={handleGenerate}
-        >
-          Generate
-        </button>
-        <button
-          className="bg-blue-500 hover:bg-blue-600 text-white py-2 px-4 ml-1 mr-1 rounded-full shadow-md hover:shadow-lg transform transition-transform duration-300 ease-in-out hover:scale-105 text-sm"
->>>>>>> 16d1ec8ab938ad13c48d0b603aca8781ed8447bf
           onClick={handleLoginClick}
         >
           Login
