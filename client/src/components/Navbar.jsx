b0e71f5ce7b25a4741b3e917e57144f676d1e3e2
EgoisticPotato <egoistic.potato@gmail.com>
2024-09-04 23:30:13 +0530

    Changed navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -18,11 +18,7 @@ const Navbar = () => {
   };
 
   return (
-<<<<<<< HEAD
     <nav className="relative bg-gray-800 text-white flex justify-between items-center px-4 py-2 overflow-hidden transition-all duration-300 ease-in-out hover:bg-gray-700">
-=======
-    <nav className="relative bg-gray-800 text-white flex justify-between items-center px-6 py-3 overflow-hidden transition-all duration-300 ease-in-out hover:bg-gray-700">
->>>>>>> b4dc0b394e6a9e28b954c0489b9c3945dd4a6895
       {/* Streak line effect */}
       <div className="absolute inset-0 flex justify-center items-center">
         <div className="absolute inset-0 bg-gradient-to-r from-transparent via-gray-900 to-transparent opacity-20" />
@@ -37,21 +33,13 @@ const Navbar = () => {
       </h1>
       <div className="flex space-x-2 z-10">
         <button
-<<<<<<< HEAD
           className="bg-blue-500 hover:bg-blue-600 text-white py-2 px-4 rounded-full shadow-md hover:shadow-lg transform transition-transform duration-300 ease-in-out hover:scale-105 text-sm"
-=======
-          className="bg-blue-500 hover:bg-blue-600 text-white py-2 px-6 rounded-full shadow-md hover:shadow-lg transform transition-transform duration-300 ease-in-out hover:scale-105"
->>>>>>> b4dc0b394e6a9e28b954c0489b9c3945dd4a6895
           onClick={handleUploadClick}
         >
           Upload
         </button>
         <button
-<<<<<<< HEAD
           className="bg-blue-500 hover:bg-blue-600 text-white py-2 px-4 rounded-full shadow-md hover:shadow-lg transform transition-transform duration-300 ease-in-out hover:scale-105 text-sm"
-=======
-          className="bg-blue-500 hover:bg-blue-600 text-white py-2 px-6 rounded-full shadow-md hover:shadow-lg transform transition-transform duration-300 ease-in-out hover:scale-105"
->>>>>>> b4dc0b394e6a9e28b954c0489b9c3945dd4a6895
           onClick={handleLoginClick}
         >
           Login
